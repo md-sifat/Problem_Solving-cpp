66720bbafd4b37010b45714888060ff9f83ea8f4
mdsifat <mdsifat7252@gmail.com>
2022-08-03 20:56:17 +0600

    added

---
+++ New folder (2)/DISCNT.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t; cin>>t;
+	while(t--){
+	    float x;
+	    cin>>x;
+	    cout<<(100-(100*(x/100)));
+	}
+	return 0;
+}
---
+++ New folder (2)/MILEAGE.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	float n,x,y,a,b;
+	cin>>n>>x>>y>>a>>b;
+	
+    if ((n*(x/a))> (n*(x/a)))
+    {
+        cout<<"PETROL"<<endl;
+    }
+    else
+    {
+        cout<<"DIESEL"<<endl;    
+    }
+    
+
+
+	return 0;
+}
