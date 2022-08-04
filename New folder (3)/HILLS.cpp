c348f155c3f23c7148bf82451a4db32a16674099
mdsifat <mdsifat7252@gmail.com>
2022-08-05 00:37:17 +0600

    added

---
+++ New folder (3)/HILLS.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int n,u,d;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    
+    cin>>n>>u>>d;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i]-arr[i+1] <= u)
+        {
+            /* code */
+        }
+        
+    }
+    
+}
