c482cd9e2ea12d1556dd60e42f4d30b95d7e9e55
mdsifat <mdsifat7252@gmail.com>
2022-08-13 16:19:30 +0600

    added

--- New folder (3)/TWOTRAINS.cpp
+++ New folder (3)/TWOTRAINS.cpp
@@ -1,24 +1,55 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
-int main(){
-    int t; cin>>t;
+int main()
+{
+    int t;
+    cin >> t;
     while (t--)
     {
-        int n; cin>>n;
+        int n;
+        cin >> n;
         int arr[n];
-        int sum=0;
-        for (int i = 0; i < n-1; i++)
+        int sum = 0;
+        for (int i = 0; i < n - 1; i++)
         {
-            cin>>arr[i];
-            sum=sum+arr[i];
+            cin >> arr[i];
+            sum += arr[i];
         }
+        int ans = 0;
 
-        cout<<sum+arr[n-2]<<endl;
+        int b = 0, a = 0, sA = 0, sB = 0, cntr = 0;
+        while (1)
+        {
+            if (sA == n - 1)
+            {
+                break;
+            }
 
-        
+            if (sB == sA)
+            {
+                cntr++;
+            }
+
+            if (sA > sB)
+            {
+                b++;
+                if (b == arr[sB])
+                {
+                    sB++;
+                    b = 0;
+                }
+            }
+
+            a++;
+            if (a == arr[sA])
+            {
+                sA++;
+                a = 0;
+            }
+        }
+        cout << sum + cntr << endl;
     }
 
     return 0;
-    
 }
