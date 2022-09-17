c7195f349c7a9a7caa7e3ff86f8149f041c55877
mdsifat <mdsifat7252@gmail.com>
2022-09-17 20:20:26 +0600

    added

---
+++ assignment/pattern4.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main(){
+    int n; scanf("%d",&n);
+    int s = (2*n-1)/2;
+    for (int i = 1; i <=n ; i++){
+        for (int j = 1; j <= s; j++){
+            printf("#");
+        }
+        for (int j = 1; j <= 2*i-1; j++){
+            (j==1 || j==2*i-1)? printf("#") : printf(" ");
+        }
+        for (int j = 1; j <= s; j++){
+            printf("#");
+        }
+        printf("\n");
+        s--;   
+    }
+    s=0;
+    for (int i = n; i >=1 ; i--){
+        for (int j = 1; j <= s; j++){
+            printf("#");
+        }
+        for (int j = 1; j <= 2*i-1; j++){
+            (j==1 || j==2*i-1)? printf("#") : printf(" ");
+        }
+        for (int j = 1; j <= s; j++){
+            printf("#");
+        }
+        printf("\n");
+        s++;   
+    }
+    
+    return 0;
+}
